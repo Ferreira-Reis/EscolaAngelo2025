278f811b867f069dbc1c9d6c5cfcc6c9420f200d
Ferreira_Reis <2016matheus2008@gmail.com>
2025-04-04 10:17:53 -0300

    Matheus

---
+++ Aula32.py
@@ -0,0 +1,12 @@
+# LISTAS, TUPLAS, CONJUNTOS E DICIONÁRIO ANINHADOS
+
+nomes = ("Sofia", "Cristal", "Matheus", "Guilherme")
+cores = ("Vermelho", "Azul", "Verde",)
+
+tupla = (nomes, cores)
+
+print(tupla)
+
+print(nomes[2])
+print(tupla[0])
+print(tupla[0][2])
