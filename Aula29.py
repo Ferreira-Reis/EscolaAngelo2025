54e9084adab578317839c03776dcc287bc387db3
Ferreira_Reis <2016matheus2008@gmail.com>
2025-04-04 08:45:44 -0300

    Matheus

---
+++ Aula29.py
@@ -0,0 +1,17 @@
+# TUPLA X LISTA 
+
+lista_vazia = []
+print(lista_vazia)
+lista_vazia.append("Bom dia")
+lista_vazia.append("Olá Mundo")
+print(lista_vazia)
+
+tupla = ()
+print(tupla)
+tupla = list (("Ana", "Paula", "Sofia"))
+tupla.append("Cristal")
+tupla = tuple(tupla)
+print(tupla)
+
+print(lista_vazia[0])
+print(tupla[3])
